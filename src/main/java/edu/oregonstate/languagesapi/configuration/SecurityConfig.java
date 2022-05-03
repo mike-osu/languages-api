a2466f82fb49a10553f5b8bd5fc84203e7abc71e
mikeacosta <mike@acosta.net>
2022-05-03 08:46:52 -0700

    initial Swagger config

--- src/main/java/edu/oregonstate/languagesapi/configuration/SecurityConfig.java
+++ src/main/java/edu/oregonstate/languagesapi/configuration/SecurityConfig.java
@@ -14,11 +14,25 @@ import java.util.List;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public static final String INDEX_PAGE = "/index.html";
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            // other public endpoints of your API may be appended to this array
+            "/index.html"
+    };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        List<String> permitAllEndpointList = Arrays.asList(INDEX_PAGE);
+        List<String> permitAllEndpointList = Arrays.asList(AUTH_WHITELIST);
 
         http.cors().and().csrf().disable()
                 .authorizeRequests(expressionInterceptUrlRegistry -> expressionInterceptUrlRegistry
