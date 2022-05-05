3d659c5e377e79b1605b11ba7964e85577f58eae
mikeacosta <mike@acosta.net>
2022-05-05 08:23:44 -0700

    Swagger UI JWT auth

--- src/main/java/edu/oregonstate/languagesapi/LanguagesApiApplication.java
+++ src/main/java/edu/oregonstate/languagesapi/LanguagesApiApplication.java
@@ -2,8 +2,10 @@ package edu.oregonstate.languagesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class LanguagesApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/edu/oregonstate/languagesapi/configuration/SwaggerConfig.java
+++ src/main/java/edu/oregonstate/languagesapi/configuration/SwaggerConfig.java
@@ -4,19 +4,59 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+                "Languages API",
+                "Spring Boot REST API Documentation",
+                "1.0",
+                "https://en.wikipedia.org/wiki/Terms_of_service",
+                new Contact("Mike Acosta",
+                        "https://eecs.oregonstate.edu/academic/online-cs-postbacc",
+                        "acostmic@oregonstate.edu"),
+                "MIT License",
+                "https://github.com/mike-osu/languages-api/blob/main/LICENSE.txt",
+                Collections.emptyList()
+        );
+    }
+
     @Bean
-    public Docket api() {
+    public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 }
